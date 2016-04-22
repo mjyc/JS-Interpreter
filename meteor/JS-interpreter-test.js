6113e1ba30e930773e6f024e8e7bb77eccb92985
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2016-04-21 17:44:39 -0700

    update to do not share variable between threads mode

--- meteor/JS-interpreter-test.js
+++ meteor/JS-interpreter-test.js
@@ -26,88 +26,8 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
   Meteor.startup(function () {
-    // Extend Interpreter obj
-    Interpreter.prototype.getGlobalScope = function() {
-      for (var i = this.stateStack.length - 1; i >= 0 ; i--) {
-        if (this.stateStack[i].scope) {
-          return this.stateStack[i].scope;
-        }
-      }
-      throw 'No scope found.';
-    };
-    Interpreter.prototype.stepThread = function() {
-      if (this.step() && this.stateStack.length > 0) {
-        if (this.parentInterpreter) {
-          if (this.stateStack[0].node.type === 'ExpressionStatement' && !this.stateStack[0].done) {
-            if (this.getScope() !== this.getGlobalScope()) {
-              console.log('this.getScope() !== this.getGlobalScope()');
-              var stopwords = [];
-              for (var key in this.getScope().properties) {
-                stopwords.push(key);
-              }
-              // console.log(stopwords);
-              // Copy global scope to local scope
-              for (var name in this.parentInterpreter.getScope().properties) {
-                if (stopwords.indexOf(name) < 0) {
-                  // console.log("local", name);
-                  this.getScope().properties[name] = this.parentInterpreter.getScope().properties[name];
-                }
-              }
-              for (var name in this.parentInterpreter.getGlobalScope().properties) {
-                if (stopwords.indexOf(name) < 0) {
-                  if (!(name in this.getScope().properties))
-                    // console.log("global", name);
-                    this.getScope().properties[name] = this.parentInterpreter.getGlobalScope().properties[name];
-                }
-              }
 
-            } else {
-              console.log('this.getScope() === this.getGlobalScope()');
-
-              // if (this.parentInterpreter.getScope() !== this.parentInterpreter.getGlobalScope()) {
-                // Copy global scope to local scope
-                for (var key in this.parentInterpreter.getScope().properties) {
-                  try {
-                    console.log('parent', this.parentInterpreter.level, key, this.parentInterpreter.getScope().properties[key].data);
-                  } catch (e) {
-                    console.log(e);
-                  }
-                }
-                for (var name in this.parentInterpreter.getScope().properties) {
-                  this.getScope().properties[name] = this.parentInterpreter.getScope().properties[name];
-                }
-                for (var name in this.parentInterpreter.getGlobalScope().properties) {
-                  if (!(name in this.getScope().properties))
-                    this.getScope().properties[name] = this.parentInterpreter.getGlobalScope().properties[name];
-                }
-              // }
-
-            }
-
-          } else if (this.stateStack[0].node.type === 'AssignmentExpression' &&
-                     this.stateStack[0].doneLeft &&
-                     this.stateStack[0].doneRight) {
-            // Copy local scope to global scope
-            // Assumes variable declaration is done only in global scope
-            if (this.getScope() === this.getGlobalScope()) {
-              this.parentInterpreter.getScope().properties[this.stateStack[0].leftSide.data] = this.stateStack[0].value;
-            } else {
-              if (!(this.stateStack[0].leftSide.data in this.getScope().properties)) {
-                this.parentInterpreter.getScope().properties[this.stateStack[0].leftSide.data] = this.stateStack[0].value;
-              }
-            }
-
-          }
-        }
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-    var code;
     var self = { interpreter: null };
-    var cnt = 0;
 
     function initApi(interpreter, scope) {
       var wrapper = function(secs) {
@@ -127,11 +47,30 @@ if (Meteor.isServer) {
       function createThreads(interpreters) {
         function runThread(interp) {
           return new Promise(function(resolve, reject) {
+            var parentScope = interp.parentInterpreter.getScope();
+            // Copy the parent scope to the thread scope
+            while (parentScope) {
+              for (var name in parentScope.properties) {
+                if (!(name in interp.getScope().properties)) {
+                  interp.getScope().properties[name] = parentScope.properties[name];
+                }
+              }
+              parentScope = parentScope.parentScope;
+            }
+            // Save the final thread scope
+            interp.finalScope = null;
             function nextStep() {
-              if (interp.stepThread()) {
-                Meteor.setTimeout(nextStep, 0);
-              } else {
-                resolve(true);
+              try {
+                if (interp.step()) {
+                  if (interp.stateStack.length === 1) {  // right before the last step
+                    interp.finalScope = interp.getScope();
+                  }
+                  Meteor.setTimeout(nextStep, 0);
+                } else {
+                  resolve(true);
+                }
+              } catch (e) {
+                reject(e);
               }
             }
             nextStep(interp);
@@ -140,6 +79,11 @@ if (Meteor.isServer) {
         return Meteor.wrapAsync(function(callback) {
           Promise.all(interpreters.map(runThread)).then(function(results) {
             console.log("wait_for_all results:", results);
+            for (var i = 0; i < interpreters.length; i++) {
+              for (var name in interpreters[i].finalScope.properties) {
+                interpreters[i].parentInterpreter.setValueToScope(name, interpreters[i].finalScope.properties[name]);
+              }
+            }
             callback(null, null);
           }).catch(function(err) {
             console.error("wait_for_all error:", err);
@@ -160,21 +104,21 @@ if (Meteor.isServer) {
           for (var i = 0; i < branches_obj.length; i++) {
             var branch = branches_obj.properties[i];
             branches.push(branch);
-            branchCode.push(code.substring(branches[i].node.start + 12, branches[i].node.end - 3));
+            branchCode.push(interpreter.code.substring(branches[i].node.start + 12, branches[i].node.end - 1));
             branchInterpreters.push(new Interpreter(branchCode[i], initApi));
-            interpreter.level = cnt;
+            branchInterpreters[i].code = branchCode[i];
             branchInterpreters[i].parentInterpreter = interpreter;
           }
-          cnt++;
-          return interpreter.createPrimitive(createThreads(branchInterpreters)());
+          createThreads(branchInterpreters)();
+          return interpreter.createPrimitive(true);
         }
       ));
     }
 
     Meteor.methods({
-      runCode: function (inputCode) {
-        code = inputCode;
+      runCode: function (code) {
         self.interpreter = new Interpreter(code, initApi);
+        self.interpreter.code = code;
         console.log("====PROGRAM START====");
         self.interpreter.run();
         console.log("====PROGRAM END====");
